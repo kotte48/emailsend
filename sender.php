2da762637fc780826541d9f0aa8a7f4b7edef373
Alexandr <72464406+kotte48@users.noreply.github.com>
2024-03-02 17:44:31 +0300

    Update sender.php

--- sender.php
+++ sender.php
@@ -5,7 +5,7 @@
     $email = $_POST['email'];
     $text = $_POST['text'];
 
-	$to = "denis.koblya@gmail.com"; 
+	$to = "kotte48@gmail.com"; 
 	$date = date ("d.m.Y"); 
 	$time = date ("h:i");
 	$from = $email;
